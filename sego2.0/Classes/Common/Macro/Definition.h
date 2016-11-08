87ee9136cf128cd54ed7f6676c21ff03948097c3
yuleicoco <1040341999@qq.com>
2016-11-08 14:41:12 +0800

    目录文件创立

---
+++ sego2.0/Classes/Common/Macro/Definition.h
@@ -0,0 +1,18 @@
+//
+//  Definition.h
+//  sego2.0
+//
+//  Created by yulei on 16/11/8.
+//  Copyright © 2016年 yulei. All rights reserved.
+//
+
+// 存放各种宏定义 存放宏的时候标注每个宏的作用
+
+
+
+#ifndef Definition_h
+#define Definition_h
+
+
+
+#endif /* Definition_h */
